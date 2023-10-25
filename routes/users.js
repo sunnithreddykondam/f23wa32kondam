a61851f0b76805c6c15778da437face92656672d
SunnithReddyKondam <142992377+sunnithreddykondam@users.noreply.github.com>
2023-10-24 19:07:23 -0500

    updated with code to know count of refresh

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+sum = 0;
+more = 0;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  more = more + 1;
+  sum += more;
+  var response = `Sum is ${sum}`;
+  res.send(response);
 });
 
 module.exports = router;
